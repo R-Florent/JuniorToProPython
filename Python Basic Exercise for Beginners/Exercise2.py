63207ccc33507f0c5a8381386d69d7512dbf3230
R-Florent <florent.real@viacesi.fr>
2024-04-23 12:49:17 +0200

    Feature : excercise 2 and his solution

--- Python Basic Exercise for Beginners/Exercise2.py
+++ Python Basic Exercise for Beginners/Exercise2.py
@@ -1,4 +1,19 @@
 
+currentNumber = 0
+previousNumber = 0
+for currentNumber in range(0, 10 , 1):
+    Sum = currentNumber + previousNumber
+    print("Current Number", currentNumber ,"Previous Number : ",previousNumber,"Sum:",Sum)
+    previousNumber = currentNumber
 
+# SOLUTION
+print("Printing current and previous number and their sum in a range(10)")
+previous_num = 0
 
-while i
+# loop from 1 to 10
+for i in range(1, 11):
+    x_sum = previous_num + i
+    print("Current Number", i, "Previous Number ", previous_num, " Sum: ", x_sum)
+    # modify previous number
+    # set it to the current number
+    previous_num = i
