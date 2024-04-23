33c334638391fd26fa6ac592f5ebda4917ba53b5
R-Florent <florent.real@viacesi.fr>
2024-04-23 16:50:52 +0200

    Feature : excercise 3 and his solution

---
+++ Python Basic Exercise for Beginners/Exercise4.py
@@ -0,0 +1,18 @@
+"""Exercise 4: Remove first n characters from a string
+
+Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+For example:
+
+remove_chars("pynative", 4) so output must be tive. Here, we need to remove the first four characters from a string.
+remove_chars("pynative", 2) so output must be native. Here, we need to remove the first two characters from a string.
+Note: n must be less than the length of the string.
+
+"""
+origanel_string = input('Enter wolrd')
+string_remove_int = input('entre number for remove input')
+taille_string = len(origanel_string)
+
+for i in range(taille_string):
+    except string_remove_int => taille_string:
+        print("Oops!  That was no valid number.  Try again...")
