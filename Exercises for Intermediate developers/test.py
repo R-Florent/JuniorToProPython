28400c6d810258c68e70b982609fb0698d64ef7f
R-Florent <florent.real@viacesi.fr>
2024-05-09 23:12:12 +0200

    Feature : fin de la fiche

--- Exercises for Intermediate developers/test.py
+++ Exercises for Intermediate developers/test.py
@@ -1,7 +1,18 @@
-sample_list = [10, 20, 60, 30, 20, 40, 30, 60, 70, 80]
-new_list = []
+# Dictionary initial
+d1 = {'A': 65, 'B': 66, 'C': 67, 'D': 68, 'E': 69, 'F': 70}
 
-for e in sample_list:
-    if sample_list.count(e) > 1 and e not in new_list :
-        new_list.append(e)
-print(new_list)
+# Liste des clés à utiliser
+l1 = ['A', 'C', 'F']
+
+# Initialisation du nouveau dictionnaire
+new_dict = {}
+
+# Itération sur chaque clé dans la liste l1
+for key in l1:
+    # Ajout de la paire clé-valeur au nouveau dictionnaire
+    new_dict[key] = d1[key]
+
+# Ajout d'une nouvelle paire clé-valeur au nouveau dictionnaire
+new_dict['G'] = 71
+
+print(new_dict)
