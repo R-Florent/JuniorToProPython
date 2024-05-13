da6df1c06cb6556805392d4a7680c9fec3fb0f08
R-Florent <florent.real@viacesi.fr>
2024-05-13 12:55:40 +0200

    feature : Exo4 & 1 l'héréditer des classe parent -> enfant

--- PYnative/OOP Exercise/Exo1.py
+++ PYnative/OOP Exercise/Exo1.py
@@ -1,5 +1,11 @@
 class Vehicle:
-    def __init__(self, name,max_speed, mileage):
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
-        self.name = name
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+    def info_vehicle(self):
+        print("LES INFO DU BUS SONT",'\n',self.name, self.max_speed, self.mileage)
---
+++ PYnative/OOP Exercise/Exo4.py
@@ -0,0 +1,21 @@
+from Exo1 import Vehicle
+
+class Bus(Vehicle):
+    def __init__(self, namebus, max_speed, mileage, capacity):
+        super().__init__(namebus, max_speed, mileage)
+        self.capacity = capacity
+
+    def Bus_info(self):
+        print('inside the bus :',self.capacity)
+
+
+    def return_capacity(self,capacity):
+        return super().seating_capacity(capacity)
+
+
+my_bus = Bus("School Volvo",100,2000,50)
+my_bus.info_vehicle()
+
+my_bus.return_capacity(my_bus.capacity)
+
+print(my_bus.seating_capacity(my_bus.capacity))
