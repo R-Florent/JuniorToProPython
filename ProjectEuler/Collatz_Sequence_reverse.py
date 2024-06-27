5cdd73a79bd28c2d32e5b6e72564fc09b0105bb7
R-Florent <florent.real@viacesi.fr>
2024-06-27 11:19:35 +0200

    PROJET EULER: collastz_sequence_reversed

--- ProjectEuler/Collatz_Sequence_reverse.py
+++ ProjectEuler/Collatz_Sequence_reverse.py
@@ -1,23 +1,25 @@
-nb_iteraction = 10
+def collastz_sequence_test(n=1,nb_iterations=10):
+    liste_de_liste = [[] for _ in range(nb_iterations)]
 
-liste_de_liste =[]
+    def generate_collatz_sequences(n, current_iteration):
+        if current_iteration == nb_iterations:
+            return
 
-for e in range(nb_iteraction):
-    liste_de_liste.append([])
-
-
-def collastz_sequence_test(n=1, nb_iteraction=10):
-    for e in liste_de_liste:
         n_pair = 2 * n
         n_impaire = (n - 1) / 3
 
-        e.append(n_pair)
-        if n_impaire%2 !=0 and n_impaire%2 == int():
-            e.append(n_impaire)
-        if nb_iteraction ==0:
-            return liste_de_liste
-        nb_iteraction -=1
-        for element in liste_de_liste[nb_iteraction]:
-            collastz_sequence_test(element)
+        liste_de_liste[current_iteration].append(n_pair)
+
+        if n_impaire % 2 != 0 and n_impaire.is_integer():
+            liste_de_liste[current_iteration].append(int(n_impaire))
+
+        generate_collatz_sequences(n_pair, current_iteration + 1)
+
+        if n_impaire % 2 != 0 and n_impaire.is_integer():
+            generate_collatz_sequences(int(n_impaire), current_iteration + 1)
+
+    generate_collatz_sequences(n, 0)
+    return liste_de_liste
+
 
 print(collastz_sequence_test())
