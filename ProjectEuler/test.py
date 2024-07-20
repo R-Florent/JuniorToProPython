b4a7332cd7d2862c0aa537cd25397f38a00d1f36
R-Florent <florent.real@viacesi.fr>
2024-07-20 12:17:48 +0200

    Projet Euler: Problem 22

--- ProjectEuler/test.py
+++ ProjectEuler/test.py
@@ -1,34 +1,15 @@
-dic = {2: 3, 3: 2, 7: 2}
-def generate_combinations(dic):
-    prime_number = list(dic.keys())
-    results = []
+def deficient():
+    #pour diviser un nombre
+    dic_des_diviseur = {}
 
-    def all_diviseur(index_dic,current):
+    for i in range(1, 29):
+        sum_des_diviseur = 0
+        for y in range(1, i // 2 + 1):
+            if i % y == 0:
+                sum_des_diviseur +=y
+            if sum_des_diviseur == i :
+                print(i,'est un nombre prafait')
 
-        if index_dic >= len(prime_number):
-            results.append(current.copy())
-            return
 
-        prime_key = prime_number[index_dic]
-        max_exponent = dic[prime_key]
 
-        for exponent in range(max_exponent+1):
-            current.append(prime_key ** exponent)
-            all_diviseur(index_dic + 1, current)
-            current.pop()
-
-    all_diviseur(0,[])
-    return results
-
-def list_all_diviseur():
-    diviseur = []
-    combinations  = generate_combinations(dic)
-    for combi in combinations:
-        product = 1
-        for number in combi:
-            product *= number
-            diviseur.append(product)
-    return diviseur
-
-for g in list_all_diviseur():
-    print(g)
+deficient()
