aae95f250cd0c4dd998bac7996894197c4cdea94
R-Florent <florent.real@viacesi.fr>
2024-07-31 12:58:49 +0200

    Projet Euler: exercise 24 1er tentative

--- Coding_Game/Clash Of Code/truc.py
+++ Coding_Game/Clash Of Code/truc.py
@@ -1,14 +1,25 @@
-import sys
-import math
+lexicographic =[0,1,2,3,4,5,6,7,8,9]
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
-n = int(input())
-x = []
-for i in input().split():
-    x = int(i)
-
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
-print(len(x[1]))
+counter =0
+for a in lexicographic:
+    for b in lexicographic:
+        if b!= a:
+            for c in lexicographic:
+                if c!= a and c!= b :
+                    for d in lexicographic:
+                        if d != a and d != b and d !=c :
+                            for e in lexicographic:
+                                if e != a and e != b and e != c and e!=d:
+                                    for f in lexicographic:
+                                        if f != a and f != b and f != c and f !=d and f !=e:
+                                            for g in lexicographic:
+                                                if g != a and g != b and g != c and g != d and g != e and g!=f:
+                                                    for h in lexicographic:
+                                                        if h != a and h != b and h != c and h != d and h != e and h!=f and h !=g:
+                                                            for i in lexicographic:
+                                                                if i != a and i != b and i != c and i != d and i != e and i!=f and i!=h and i!=g and i!=h:
+                                                                    for j in lexicographic:
+                                                                        if j != a and j != b and j != c and j != d and j != e and j!=f and j!=h and j!=g and j!=h and j !=i:
+                                                                            counter +=1
+                                                                            if counter == 1_000_000:
+                                                                                print([a,b,c,d,e,f,g,h,i,j])
