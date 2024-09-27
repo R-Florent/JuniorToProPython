bad1713466e6c71e376a26fc936fc90205ae1e34
R-Florent <florent.real@viacesi.fr>
2024-09-27 16:09:59 +0900

    Project Euler: exercise 36 encout leger default

--- ProjectEuler/Test_for_Project_Euler.py
+++ ProjectEuler/Test_for_Project_Euler.py
@@ -1,13 +1,37 @@
-list_resulte = []
-res = 0
-for a in range(2, 5+1):
-    for b in range(2, 5+1):
+def crible_eratosthene(limite):
+    # Initialiser une liste de booléens : True signifie "potentiellement premier"
+    est_premier = [True] * (limite + 1)
+    est_premier[0] = est_premier[1] = False  # 0 et 1 ne sont pas premiers
 
-        res = a ** b
-        if res in list_resulte:
-            print('cette valeur est déja dans la liste', res)
-        list_resulte.append(res)
+    # Parcourir les nombres pour marquer les multiples
+    for i in range(2, int(limite**0.5) + 1):
+        if est_premier[i]:
+            for multiple in range(i * i, limite + 1, i):
+                est_premier[multiple] = False
 
-list_resulte = sorted(list_resulte)
+    # Retourner une liste des nombres premiers
+    return [num for num, premier in enumerate(est_premier) if premier]
 
-print(list_resulte)
+# Fonction pour vérifier si un nombre est premier
+def est_premier(nombre, crible):
+    return nombre in crible
+
+
+def is_truncatable(n):
+    str_n = str(n)
+    str_n_reverse = str_n[::-1]
+    for i in range(len(str_n)):
+        if not est_premier(int(str_n[i:]), crible):
+            continue
+        else:
+            print(n,"est un cifrepremier")
+
+    for i in range(len(str_n_reverse)):
+        if not est_premier(str_n_reverse[:i], crible):
+            continue
+
+
+limite = 1000
+crible = crible_eratosthene(limite)
+for e in crible:
+    is_truncatable(e)
