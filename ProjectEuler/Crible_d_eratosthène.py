afde4c482b69cd43aba62279671d66b6a900dfce
R-Florent <florent.real@viacei.fr>
2026-04-11 01:18:42 +0900

    Exercise : Crible d'eratosthène

---
+++ ProjectEuler/Crible_d_eratosthène.py
@@ -0,0 +1,11 @@
+def crible_eratosthène_2026(n):
+    L = [True] * n+1
+    L[0] = False
+    L[1] = False
+    for i in range(2,n):
+        if L[i]:
+            for j in range(i*i,n,i):
+                L[j] = False
+    return [e for e in range(n) if L[e]==True]
+
+print(crible_eratosthène_2026(50))
